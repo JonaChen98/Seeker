53b9bba95adc0ed8402293cee659449802222221
Amy Ghotra <amy.ghotra8@gmail.com>
2021-04-15 18:03:51 -0400

    changes

--- seeker/src/App.js
+++ seeker/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import LandingPage from './Landing_Page/LandingPage'
 import LandingAbout from './Landing_Page/LandingAboutUs'
+// import App2 from './Landing_Page/Quotes'
+import Images from './Landing_Page/Images'
 
 function App() {
   return (
     <>
-      <LandingPage  className="page_top" />
+      <LandingPage className="page_top" />
       <LandingAbout />
+      <Images />
+      {/* <App2 /> */}
     </>
   );
 }
--- seeker/src/Landing_Page/LandingPage.js
+++ seeker/src/Landing_Page/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../index.css';
+import './LandingTop.css';
 import new_york from '../images/newyork.jpeg'
 
 export default function LandingPage()
@@ -11,7 +11,10 @@ export default function LandingPage()
                 <p className ="landing_seeker" id="seeker_landing">Seeker</p>
                 <button id ="login_button">Login</button>
                 <button id ="sign_up">Sign Up</button>
-                <h1 id="landing_quote">Everything's better with friends</h1>
+                <div id="quote_subquote">
+                    <h1 id="landing_quote">Everything's better with friends</h1>
+                    <h3 id="dare">Seeker was created to improve (dare we say, maybe even resurrect) your social life.</h3>
+                </div>
                 <img id="new_york" src={new_york}/>
             </div>
         </>
