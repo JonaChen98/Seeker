3188b1d91f7efc7e7003870525d08f0f94bdd795
[Ramiz Faragalla] <[ramezkelada66@gmail.com]>
2021-04-15 20:29:32 -0400

    user model

---
+++ api/models/user.js
@@ -0,0 +1,54 @@
+'use strict';
+const { Model } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {}
+
+  User.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [3, 250],
+        notEmpty: true,
+      }
+    },
+
+    // email must be unique
+    email: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [3, 250],
+          notEmpty: true,
+        },
+        unique: true,
+    },
+
+    password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [3, 250],
+          notEmpty: true,
+        },
+    },
+
+    about: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [3, 250],
+          notEmpty: true,
+        },
+        unique: true,
+    },
+
+  }, {
+    sequelize,
+    modelName: 'user'
+  });
+
+  User.associate = (models) => {
+    // associations can be defined here
+  };
+
+  return User;
+};
