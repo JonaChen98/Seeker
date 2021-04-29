c7cc8a6d6c34668838bb5bad3e78321e85850a82
Amy Ghotra <amy.ghotra8@gmail.com>
2021-04-29 16:42:50 -0400

    added social media information and css edits

--- seeker/src/Pages/UserProfile/UserInfo.js
+++ seeker/src/Pages/UserProfile/UserInfo.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './userinfo.css'
 import people from '../../images/threepeople.jpeg'
+import twitter from '../../images/twitter.png'
+import facebook from '../../images/facebook.png'
+import instagram from '../../images/instagram.png'
 
 import Fade from 'react-reveal/Fade';
 
@@ -8,19 +11,22 @@ class UserInfo extends React.Component {
     constructor() {
         super();
         this.state={
-            profile_picture:[],
+            profile_picture:"",
             name:"",
+            instagram_link:"https://www.instagram.com",
+            facebook_link:"",
+            twitter_link:"",
             gender:"",
             drinks:false,
             devils_lettuce: false,
             drugs:false,
             bio:""
         }
-        this.goToSettingsPage = this.goToSettingsPage.bind(this)
+        this.goToEditPage = this.goToEditPage.bind(this)
         this.logout_req = this.logout_req.bind(this)
     }
 
-    goToSettingsPage() {
+    goToEditPage() {
         alert("now going to settings page")
     }
 
@@ -34,11 +40,22 @@ class UserInfo extends React.Component {
                 {/* <Fade right> */}
                 <div id="user_info_container">
                     <Fade>
-                        <button onClick={this.goToSettingsPage} id="profile_settings_button">settings</button>
+                        <button onClick={this.goToEditPage} id="profile_edit_button">edit</button>
                         <button onClick={this.logout_req} id="profile_logout_button">logout</button>
                         <div className="user_info_item">
                             <img id="user_image" src={people} />
                             <h4 id="user_name"> Katy Johnson</h4> 
+                            <div id="social_media user_info_item">
+                                {this.state.twitter_link.length ? 
+                                    <a href={this.state.twitter_link}><img className="sm_icons" src={twitter}/></a> : null
+                                }
+                                {this.state.instagram_link.length ? 
+                                    <a href={this.state.instagram_link}><img className="sm_icons" src={instagram}/></a> : null
+                                }
+                                {this.state.facebook_link.length ? 
+                                    <a href={this.state.facebook_link}><img className="sm_icons" src={facebook}/></a> : null
+                                }
+                            </div>
                             <div id="user_info">
                                 <h4 id="user_about_heading"> about me </h4> 
                                 <p id="user_gender">gender: female </p>
